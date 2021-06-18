135a1161b58e691289e63ee61c06e8dcac6b99f6
Marco <nhytros@gmail.com>
2021-06-18 07:49:57 +0200

    Update ReadableServiceProvider.php

--- src/ReadableServiceProvider.php
+++ src/ReadableServiceProvider.php
@@ -29,52 +29,52 @@ class ReadableServiceProvider extends ServiceProvider
         $n = 'Readable';
 
         // Numbers
-        Blade::directive($n . 'Number', function ($data) {
+        Blade::directive($n.'Number', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getNumber($data); ?>";
         });
 
-        Blade::directive($n . 'HumanNumber', function ($data) {
+        Blade::directive($n.'HumanNumber', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getHumanNumber($data); ?>";
         });
 
-        Blade::directive($n . 'NumberToString', function ($data) {
+        Blade::directive($n.'NumberToString', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getNumberToString($data); ?>";
         });
 
-        Blade::directive($n . 'Decimal', function ($data) {
+        Blade::directive($n.'Decimal', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDecimal($data); ?>";
         });
 
-        Blade::directive($n . 'DecInt', function ($data) {
+        Blade::directive($n.'DecInt', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDecInt($data); ?>";
         });
 
         // Date and Time
-        Blade::directive($n . 'Date', function ($data) {
+        Blade::directive($n.'Date', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDate($data); ?>";
         });
 
-        Blade::directive($n . 'Time', function ($data) {
+        Blade::directive($n.'Time', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getTime($data); ?>";
         });
 
-        Blade::directive($n . 'DateTime', function ($data) {
+        Blade::directive($n.'DateTime', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDateTime($data); ?>";
         });
 
-        Blade::directive($n . 'DiffDateTime', function ($data) {
+        Blade::directive($n.'DiffDateTime', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDiffDateTime($data); ?>";
         });
 
-        Blade::directive($n . 'TimeLength', function ($data) {
+        Blade::directive($n'TimeLength', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getTimeLength($data); ?>";
         });
 
-        Blade::directive($n . 'DateTimeLength', function ($data) {
+        Blade::directive($n.'DateTimeLength', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getDateTimeLength($data); ?>";
         });
 
-        Blade::directive($n . 'Size', function ($data) {
+        Blade::directive($n.'Size', function ($data) {
             return "<?php echo Pharaonic\Laravel\Readable\Readable::getSize($data); ?>";
         });
     }
